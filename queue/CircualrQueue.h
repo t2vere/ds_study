e6dfd6b845aebed83918bdee533c7a1b89751fe9
t2vere <t2vere@gmail.com>
2018-01-30 17:55:21 +0900

    circular queue 헤더 추가

---
+++ queue/CircualrQueue.h
@@ -0,0 +1,34 @@
+/*
+ * CircualrQueue.h
+ *
+ *  Created on: 2018. 1. 30.
+ */
+
+#ifndef QUEUE_CIRCUALRQUEUE_H_
+#define QUEUE_CIRCUALRQUEUE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tagNode {
+	int data;
+} Node;
+
+typedef struct tagCircularQueue {
+	int capa;
+	int front;
+	int rear;
+
+	Node* nodes;
+} CircularQueue;
+
+void CQ_createQueue(CircularQueue** queue);
+void CQ_destroyQueue(CircularQueue* queue);
+Node* CQ_createNode(int data);
+void CQ_destroyNode(Node* node);
+void CQ_enqueue(CircularQueue* queue, Node* newNode);
+Node* CQ_dequeue(CircularQueue* queue);
+int CQ_isEmpty(CircularQueue* queue);
+int CQ_isFull(CircularQueue* queue);
+
+#endif /* QUEUE_CIRCUALRQUEUE_H_ */
---
+++ queue/CircularQueue.c
@@ -0,0 +1,35 @@
+/*
+ * CircularQueue.c
+ *
+ *  Created on: 2018. 1. 30.
+ */
+
+#include "CircualrQueue.h"
+
+void CQ_createQueue(CircularQueue** queue) {
+
+}
+
+void CQ_destroyQueue(CircularQueue* queue) {
+
+}
+
+Node* CQ_createNode(int data) {
+	return NULL;
+}
+
+void CQ_destroyNode(Node* node) {}
+
+void CQ_enqueue(CircularQueue* queue, Node* newNode) {}
+
+Node* CQ_dequeue(CircularQueue* queue) {
+	return NULL;
+}
+
+int CQ_isEmpty(CircularQueue* queue) {
+	return 0;
+}
+
+int CQ_isFull(CircularQueue* queue) {
+	return 0;
+}
