8604da63e814c865455a496fe963f47595f919c1
t2vere <t2vere@gmail.com>
2018-01-31 18:40:32 +0900

    수정

--- tree/ExpTree.c
+++ tree/ExpTree.c
@@ -7,3 +7,63 @@
 
 
 #include "ExpTree.h"
+
+EtNode* ET_createNode(char data) {
+	EtNode* node = (EtNode*) malloc(sizeof(EtNode));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+
+	return node;
+}
+
+void ET_destroyNode(EtNode* node) {
+	free(node);
+}
+
+void ET_destroyTree(EtNode* node) {
+	if (node == NULL) {
+		return;
+	}
+
+	ET_destroyTree(node->left);
+	ET_destroyTree(node->right);
+	ET_destroyNode(node);
+}
+
+void ET_preOrderPrintTree(EtNode* node) {
+	if (node == NULL) {
+		return;
+	}
+
+
+}
+
+void ET_postOrderPrintTree(EtNode* node) {
+	if (node == NULL) {
+		return;
+	}
+
+
+}
+
+void ET_inOrderPrintTree(EtNode* node) {
+	if (node == NULL) {
+		return;
+	}
+
+	printf(" (");
+	ET_inOrderPrintTree(node->left);
+	printf(" %c", node->data);
+	ET_inOrderPrintTree(node->right);
+	printf(" )");
+}
+
+void ET_buildExpressionTree(char* postfixExpression, EtNode** node) {
+
+}
+
+double ET_evaluate(EtNode* tree) {
+	return 0;
+}
+
--- tree/ExpTree.h
+++ tree/ExpTree.h
@@ -11,4 +11,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct tagEtNode {
+	struct tagEtNode* left;
+	struct tagEtNode* right;
+	char data; // 한 자리만 처리 가능
+
+} EtNode;
+// 연산 종류 : create, destroy, print(3가지 종류), build, evaluate
+
+EtNode* ET_createNode(char data);
+void ET_destroyNode(EtNode* node);
+void ET_destroyTree(EtNode* node);
+
+void ET_preOrderPrintTree(EtNode* node);
+void ET_postOrderPrintTree(EtNode* node);
+void ET_inOrderPrintTree(EtNode* node);
+
+void ET_buildExpressionTree(char* postfixExpression, EtNode** node);
+double ET_evaluate(EtNode* tree);
+
 #endif /* TREE_EXPTREE_H_ */
