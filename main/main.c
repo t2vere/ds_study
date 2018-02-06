5f8707e087c2fad38b40216d5226183936ad8cf7
t2vere <t2vere@gmail.com>
2018-02-06 10:11:51 +0900

    test용 main

---
+++ main/main.c
@@ -0,0 +1,36 @@
+/*
+ * main.c
+ *
+ *  Created on: 2018. 2. 6.
+ *      Author: user
+ */
+
+//#include "../queue/CircualrQueue.h"
+//#include "../queue/LinkedQueue.h"
+//#include "../sort/MySort.h"
+//#include "../tree/DisjointSet.h"
+//#include "../tree/ExpTree.h"
+//#include "../tree/LCRSTree.h"
+//#include "../tree/SBTree.h"
+
+int main(void) {
+
+	// list
+
+	// stack
+
+	// queue
+//	testLinkedQueue();
+//
+//	// tree
+//	testSbTree();
+//	testLcrsTree();
+//	testExpTree();
+//	testDisjointSet();
+
+	// sort
+//	testSort();
+
+	return 0;
+}
+
