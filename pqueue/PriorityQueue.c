0ed0f5bfaed28653015022480a9c55cc6a26769d
t2vere <t2vere@gmail.com>
2018-02-21 15:23:56 +0900

    pq header 추가

--- pqueue/PriorityQueue.c
+++ pqueue/PriorityQueue.c
@@ -6,3 +6,36 @@
  */
 
 
+#include "PriorityQueue.h"
+
+PriorityQueue* createQueue(int initSize) {
+	return NULL;
+}
+
+void destroyQueue(PriorityQueue* pq) {
+
+}
+
+void enqueue(PriorityQueue* pq, PQNode newData) {
+
+}
+
+void dequeue(PriorityQueue* pq, PQNode root) {
+
+}
+
+int getParent(int index) {
+	return 0;
+}
+
+int getLeftChild(int index) {
+	return 0;
+}
+
+void swapNodes(PriorityQueue* pq, int index1, int index2) {
+
+}
+
+int isEmpty(PriorityQueue* pq) {
+	return 0;
+}
--- pqueue/PriorityQueue.h
+++ pqueue/PriorityQueue.h
@@ -8,6 +8,31 @@
 #ifndef PQUEUE_PRIORITYQUEUE_H_
 #define PQUEUE_PRIORITYQUEUE_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
 
+
+typedef int PriorityType;
+
+typedef struct tagPQNode {
+	PriorityType priority;
+	void* data;
+} PQNode;
+
+typedef struct tagPriorityQueue {
+	PQNode* nodes;
+	int capa;
+	int usedSize;
+} PriorityQueue;
+
+PriorityQueue* createQueue(int initSize);
+void destroyQueue(PriorityQueue* pq);
+void enqueue(PriorityQueue* pq, PQNode newData);
+void dequeue(PriorityQueue* pq, PQNode root);
+int getParent(int index);
+int getLeftChild(int index);
+void swapNodes(PriorityQueue* pq, int index1, int index2);
+int isEmpty(PriorityQueue* pq);
 
 #endif /* PQUEUE_PRIORITYQUEUE_H_ */
